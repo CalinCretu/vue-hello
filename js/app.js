0dc80b486c641266c4146e1a1c593ab5c526f958
CalinCretu <calincretu1412@gmail.com>
2023-10-23 16:46:31 +0200

    ADD - experiments

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@ const createApp = Vue.createApp;
 const param = {
   data: function () {
     return {
+      num: 3,
+      numeri: 56,
       message: 'Hello Vue :)',
       content: 'Welcome to Vue!!!',
+      member: ['Giacinto', 'Giangianni', 'Giangiacomo'],
+      className: 'border',
+      colorWhite: 'white',
+      image: 'bg-image'
     }
+  },
+  methods: {
+    onClick: function () {
+      this.num++
+    },
   }
-};
+}
 const app = createApp(param);
 app.mount('#app');
